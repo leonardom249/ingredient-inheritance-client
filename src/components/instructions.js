f533b78356b4e3627bbe7369e49ec8ad126dfa50
leonardom249 <leonardo.megan@gmail.com>
2018-04-26 13:13:42 -0500

    updated instructions to have a delete part

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -6,22 +6,26 @@ export default function Instructions (){
         <div>
             <h2>Things You Can Do:</h2>
             <ul>
-                <li>Create and save your own recipes for people to enjoy</li>
-                <li>Do that by:
+                <li><strong>Create</strong> and save your own recipes for people to enjoy
                     <ol>
                         <li>Click the "+Create New Recipe" button, or just type in the blank form</li>
                         <li>Fill out the form with all your ingredients, a title, and recipe steps</li>
                         <li>Hit the "Save" button at the bottom</li>
                     </ol>
                 </li>
-                <li>View a list of your created recipes under the "My Family Recipe" section</li>
-                <li>Click on a title under "My Family Recipes" to view it in the editor</li>
-                <li>Edit your recipe by:
+                <li><strong>View a list</strong> of your created recipes under the "My Family Recipe" section</li>
+                <li><strong>View/Edit</strong> your recipe by:
                     <ol>
                         <li>Clicking the title under the "My Family Recipes section</li>
                         <li>Updating whichever fields you would like updated</li>
                         <li>Click the "Save" button</li>
                     </ol>
+                </li>
+                <li>
+                   <strong> Delete</strong> an old recipe by clicking the "X" button next to the recipe you'd like to delete
+                </li>
+                <li>
+                    <strong>*WARNING: At this time, hitting the "X" button will PERMANENTLY DELETE your recipe</strong>
                 </li>
             </ul>
             <InstructionButton/>
