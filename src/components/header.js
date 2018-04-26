22dae705e2a0ac5b0bc54098b37e46aa8c81e87a
leonardom249 <leonardo.megan@gmail.com>
2018-04-26 15:50:03 -0500

    starting basic css for ideas

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import '../component-css/header.css';
 
 export default function Header (){
 
     return(
         <div>
             <header>
-                <span><em>A place to store recipes passed down through generations...</em></span>
-                <h1>Ingredient Inheritance</h1>
-                <span><em>for the sake of food and family.</em></span>
+                <span className='header'><em>A place to store recipes passed down through generations...</em></span>
+                <h1 className='header'>Ingredient Inheritance</h1>
+                <span className='header'><em>for the sake of food and family.</em></span>
 
             </header>
         </div>
