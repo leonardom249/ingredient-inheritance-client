fd802e377a8529b93b2d82fd2e2408c6ef05823a
leonardom249 <leonardo.megan@gmail.com>
2018-07-13 12:46:30 -0500

    login and register in nav for landing page

--- src/actions/landing-page.js
+++
@@ -1,9 +0,0 @@
-export const GO_TO_LOGIN='GO_TO_LOGIN'
-export const goToLogin=()=>({
-    type:GO_TO_LOGIN
-});
-
-export const GO_TO_REGISTRATION='GO_TO_REGISTRATION'
-export const goToRegistration=()=>({
-    type:GO_TO_REGISTRATION
-});
--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import recipeReducer from './reducers/recipe';
-import landingPageReducer from './reducers/landing-page';
 import authReducer from './reducers/auth-reducer';
 
 import {reducer as formReducer} from 'redux-form';
@@ -11,7 +10,6 @@ const store = createStore( combineReducers({
     form: formReducer,
     recipe: recipeReducer,
     auth: authReducer,
-    landingPage: landingPageReducer
 }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
  applyMiddleware(thunk));
 
